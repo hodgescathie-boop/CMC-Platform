91d759ac773f88c75caa009fca836be0a5ebcfe7
hodgescathie-boop <hodgescathie@gmail.com>
2025-08-30 09:32:16 -0500

    Update urls.py

--- cleaning_platform/urls.py
+++ cleaning_platform/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from ops.views import home, estimate
+from ops.views import home, estimate, estimate_thanks
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home, name="home"),
     path("estimate/", estimate, name="estimate"),
+    path("estimate/thanks/", estimate_thanks, name="estimate_thanks"),
 ]
